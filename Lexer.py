470a68f23035620e6b2dac4fa1c32ad82966b7eb
David Carrion Beniquez <david.carrion@upr.edu>
2019-04-16 17:21:37 -0400

    Merge branch 'LexerRules' of /Users/davidcarrionbeniquez/PycharmProjects/Test_It with conflicts.

--- Lexer.py
+++ Lexer.py
@@ -1,17 +1,6 @@
 import ply.lex as lex
 import ply.yacc as yacc
 
-tokens = [
-    'ID',
-    'NUM',
-    'STRING',
-    'OPENPAR',
-    'CLOSEPAR',
-    'OPENBRACKET',
-    'CLOSEBRACKET',
-    'COLON'
-
-]
 
 reserved = {
     'test': 'TEST',
@@ -28,7 +17,62 @@ reserved = {
     'positive': 'POSITIVE',
     'reverse': 'REVERSE',
     'min': 'MIN',
-    'max': 'MAX'
+    'max': 'MAX',
+    'define': 'DEFINE',
+    'create': 'CREATE',
+    'tester': 'TESTER'
+
 }
 
+tokens = [
+    'ID',
+    'NUM',
+    'STRING',
+    'OPENPAR',
+    'CLOSEPAR',
+    'OPENBRACKET',
+    'CLOSEBRACKET',
+    'COLON'
 
+] + list(reserved.values())
+
+#rules for regular expressions
+
+#parenthesis
+t_OPENPAR= r'\('
+t_CLOSEPAR= r'\)'
+
+#brackets
+t_OPENBRACKET= r'\['
+t_CLOSEBRACKET= r'\]'
+
+#colon
+t_COLON= r'\;'
+
+
+#numbers
+def t_NUM(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+#ID
+def t_ID(t):
+    r'[a-zA-Z_][a-zA-Z_0-9]*'
+    t.type = reserved.get(t.value,'ID')
+    return t
+
+#line number?
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+#espacios y tabs
+t_ignore  = ' \t'
+
+#error handling
+def t_error(t):
+    print("Illegal character '%s'" % t.value[0])
+    t.lexer.skip(1)
+
+#build the lexer
+lexer = lex.lex()
